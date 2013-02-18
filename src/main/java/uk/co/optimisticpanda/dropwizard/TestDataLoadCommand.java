be9c582187e98ca928b4ea8d7bbaba071e2fc341
Andrew Lee <plasma147@gmail.com>
2013-02-18 23:45:45 +0000

    using daofactory to hide jdbi implementation from data gen task

--- src/main/java/uk/co/optimisticpanda/dropwizard/TestDataLoadCommand.java
+++ src/main/java/uk/co/optimisticpanda/dropwizard/TestDataLoadCommand.java
@@ -1,8 +1,9 @@
 package uk.co.optimisticpanda.dropwizard;
 
+import static uk.co.optimisticpanda.dropwizard.dao.QuestionEventDao.Change.CREATE;
+import static uk.co.optimisticpanda.dropwizard.dao.QuestionEventDao.Change.UPDATE;
 import net.sourceforge.argparse4j.inf.Namespace;
 
-import org.skife.jdbi.v2.DBI;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,12 +13,12 @@ import uk.co.optimisticpanda.dropwizard.dao.QuestionEventDao.Change;
 import uk.co.optimisticpanda.dropwizard.domain.Question;
 import uk.co.optimisticpanda.dropwizard.domain.builder.EventGenerator;
 import uk.co.optimisticpanda.dropwizard.event.Event;
+import uk.co.optimisticpanda.dropwizard.util.DaoFactory;
 import uk.co.optimisticpanda.dropwizard.util.RandomDateProvider;
 
 import com.yammer.dropwizard.Service;
 import com.yammer.dropwizard.cli.EnvironmentCommand;
 import com.yammer.dropwizard.config.Environment;
-import com.yammer.dropwizard.jdbi.DBIFactory;
 
 public class TestDataLoadCommand extends EnvironmentCommand<DemandoConfiguration> {
 
@@ -29,24 +30,23 @@ public class TestDataLoadCommand extends EnvironmentCommand<DemandoConfiguration
 
 	@Override
 	protected void run(Environment environment, Namespace namespace, DemandoConfiguration configuration) throws Exception {
-		DBI jdbi = new DBIFactory().build(environment, configuration.getDatabaseConfiguration(), "mysql");
-		QuestionDao questionDao = jdbi.onDemand(QuestionDao.class);
-		QuestionEventDao eventDao = jdbi.onDemand(QuestionEventDao.class);
+		DaoFactory factory= new DaoFactory(environment, configuration);
+		QuestionDao questionDao = factory.getDao(QuestionDao.class);
+		QuestionEventDao eventDao = factory.getDao(QuestionEventDao.class);
 
 		EventGenerator eventGenerator = new EventGenerator();
 		new RandomDateProvider().provide(eventGenerator);
 
-		System.out.println();
 		log.info("Processing {} events!", eventGenerator.getEvents().size());
 		for (Event<Question<?>, Change> event : eventGenerator.getEvents()) {
 			log.info("Inserting event: {}", event.getId());
 			eventDao.insert(event.getEventType(), event);
 			
-			if (event.getEventType().equals(Change.CREATE)) {
+			if (event.getEventType() == CREATE) {
 				log.info("Creating Question: {}", event.getPayload().getId());
 				questionDao.insert(event.getPayload().getType(), event.getPayload());
 			
-			} else if (event.getEventType().equals(Change.UPDATE)) {
+			} else if (event.getEventType() == UPDATE) {
 				log.info("Updating Question: {}", event.getPayload().getId());
 				questionDao.update(event.getPayload().getType(), event.getPayload());
 			}
